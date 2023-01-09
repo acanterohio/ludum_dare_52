6f6fe811fe96feb8b83a945bc6edbfc3af55f206
Socsob <acanterohio@gmail.com>
2023-01-09 12:54:27 -0500

    Added hands and feet to color controller

--- Assets/DonorColorController.cs
+++ Assets/DonorColorController.cs
@@ -7,6 +7,9 @@ public class DonorColorController : MonoBehaviour
     [SerializeField] MeshRenderer hat; //(0)
     [SerializeField] MeshRenderer person; //skin (0) and shirt (3)
     [SerializeField] MeshRenderer stripe; //(0)
+    [SerializeField] MeshRenderer hand1, hand2;
+    [SerializeField] MeshRenderer foot1, foot2;
+
 
     public void setMaterials(Material skinMat, Material shirtMat, Material stripeMat, Material hatMat)
     {
@@ -15,5 +18,9 @@ public class DonorColorController : MonoBehaviour
         person.materials[3].color = shirtMat.color;
         stripe.materials[0].color = stripeMat.color;
         hat.materials[0].color = hatMat.color;
+        hand1.material.color = skinMat.color;
+        hand2.material.color = skinMat.color;
+        foot1.material.color = shirtMat.color;
+        foot2.material.color = shirtMat.color;
     }
 }
